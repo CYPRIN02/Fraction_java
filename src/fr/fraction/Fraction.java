f06971008424b60bd9420a07a9f6997cf25d6764
Utilisateur <user@debian.arcahe.ovh>
2019-10-17 11:53:12 +0200

    first commit

---
+++ src/fr/fraction/Fraction.java
@@ -0,0 +1,12 @@
+package fr.fraction;
+
+public final class Fraction {
+	private int _denominateur;
+	private int _numerateur;
+	
+	public Fraction(int denominateur, int numerateur) {
+		this._denominateur = denominateur;
+		this._numerateur= numerateur;
+	}
+	
+}
