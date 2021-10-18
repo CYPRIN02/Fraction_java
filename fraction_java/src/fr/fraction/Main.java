aac7f81b89fcee10e21d5417f0921e8647a22f4f
uvsq21805123 <princy.rasoloarivony@ens.uvsq.fr>
2021-10-19 00:38:25 +0200

    rajout de la manipulation de la fraction classe

--- fraction_java/src/fr/fraction/Main.java
+++ fraction_java/src/fr/fraction/Main.java
@@ -1,17 +1,39 @@
 package fr.fraction;
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 
 		// TODO Auto-generated method stub
-		Fraction f = new Fraction(1, 2);
+		Fraction f = new Fraction(11, 2);
 		Fraction e = new Fraction(4, 3);
 		
-		System.out.println(f.add(e).toString());
+		System.out.println("Addition 2 fractions : "+ "("+f.toString()+ ")+(" + e.toString()+ ")=(" + f.add(e).toString()+")");
 		
-		System.out.println(f.virguleFlottante());
-		System.out.println(f.toString());
+		System.out.println("Virgule flottante : " + f.virguleFlottante());
+		
+		System.out.println("To string : " + f.toString());
+		
+		System.out.println("Hashcode : " + f.hashCode());
+		
+		@SuppressWarnings("resource")
+		Scanner myinput = new Scanner(System.in);
+		System.out.println("Specifer ton numérateur : ");
+		int num = myinput.nextInt();
+		System.out.println("Specifer ton dénominateur : ");
+		int denom = myinput.nextInt();
+		
+		Fraction create_f = new Fraction(num, denom);
+		System.out.println("Voici les résultats : ");
+		
+		System.out.println("Virgule flottante : " + create_f.virguleFlottante());
+		
+		System.out.println("To string : " + create_f.toString());
+		
+		System.out.println("Hashcode : " + create_f.hashCode());
+		
+		
+		
 	}
 
 }
